e5702b8d9b86b3fff047a279b6ec73362832bd57
harleymacedo <harley.ip@gmail.com>
2022-04-27 14:46:22 -0300

    Estrutura inicial montada

---
+++ src/Home.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, TextInput } from 'react-native';
+
+export default function Home(navigation) {
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+
+    const navegarProfessor = () => {
+        navigation.navigate('Professor');
+    }
+
+    const navegarDisciplina = () => {
+        navigation.navigate('Disciplina');
+    }
+
+    return (
+        <View style={styles.container}>
+            <TextInput></TextInput>
+            <TextInput></TextInput>
+            <TouchableOpacity style={styles.button1} onPress={navegarProfessor}><Text style={styles.text1}>Editar</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button1} onPress={navegarDisciplina}><Text style={styles.text1}>Postar</Text></TouchableOpacity>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#124050',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text1: {
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    button1: {
+        backgroundColor: '#aaffff',
+        width: 100,
+        height: 30,
+        marginBottom: 20
+    }
+})
---
+++ src/Professor.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, TextInput } from 'react-native';
+
+export default function Professor() {
+
+    const entrar = () => {
+
+    }
+
+    return (
+        <View style={styles.container}>
+            <TextInput></TextInput>
+            <TextInput></TextInput>
+            <TouchableOpacity><Text>Professor</Text></TouchableOpacity>
+            <TouchableOpacity><Text>Disciplina</Text></TouchableOpacity>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#124050',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text1: {
+
+    },
+    button1: {
+
+    }
+})
